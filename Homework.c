1653675c05301a21d11100e191cfb39f6672a46f
Simercher <62394051+Simercher@users.noreply.github.com>
2022-01-07 22:19:12 +0800

    Add files via upload

--- Homework.c
+++ Homework.c
@@ -1,12 +1,12 @@
-# include "stdio.h"
-# include "stdlib.h"
-# include "string.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
 
-typedef struct stackNode 
+typedef struct stackNode
 {
     char data[20];
     struct stackNode *next;
-}StackNode;
+} StackNode;
 typedef StackNode *StackNodePtr;
 
 void push(StackNodePtr *topPtr, char value[20]);
@@ -26,11 +26,11 @@ int main()
         // switch(choice)
         // {
         //     case 1:// push function
-                // printf("input push item : ");
-                // scanf("%s", item);
-                // push(&stackList, item);
-                // printStack(stackList);
-                // break;
+        // printf("input push item : ");
+        // scanf("%s", item);
+        // push(&stackList, item);
+        // printStack(stackList);
+        // break;
         //     case 2:// pop function
         //         pop(&stackList);
         //         printStack(stackList);
@@ -43,7 +43,6 @@ int main()
         push(&stackList, item);
         printStack(stackList);
     }
-    
 
     return 0;
 }
@@ -57,19 +56,26 @@ void push(StackNodePtr *topPtr, char value[])
 {
     StackNodePtr newPtr = malloc(sizeof(StackNode));
 
-    if(newPtr != NULL)
+    if (newPtr != NULL)
     {
-        for (int i = 0; i < strlen(value); i++)
+        int i = 0;
+        for (i = 0; i < strlen(value); i++)
         {
             newPtr->data[i] = value[i];
         }
+        // printf("%d\n", i);
+        if (strlen(value) < strlen(newPtr->data))
+        {
+            newPtr->data[i] = '\0';
+        }
+        // printf("%s\n", newPtr->data);
         newPtr->next = *topPtr;
         *topPtr = newPtr;
-    }else
+    }
+    else
     {
         printf("%s not inserted. No memory available.\n", value);
     }
-
 }
 
 void pop(StackNodePtr *topPtr)
@@ -84,9 +90,11 @@ void printStack(StackNodePtr currentPtr)
     if (isEmpty(currentPtr))
     {
         puts("List is empty.\n");
-    }else
+    }
+    else
     {
         puts("The List is: ");
+        // printf("The List is: \n");
 
         while (currentPtr != NULL)
         {
--- tree.c
+++ tree.c
@@ -1,48 +1,59 @@
-# include <stdio.h>
-# include <string.h>
-# include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-typedef struct treeNode 
+typedef struct treeNode
 {
     struct treeNode *left;
-    int data;
+    int datanumber;
+    char name[20];
+    int score[3];
     struct treeNode *right;
-}TreeNode;
+
+} TreeNode;
+
 typedef TreeNode *TreeNodePtr;
 
-void insertNode(TreeNodePtr *Treeptr, int data);
-void searchNode(TreeNodePtr *Treeptr, int data);
+void insertNode(TreeNodePtr *Treeptr, int datanum, char[], int[]);
+int searchNode(TreeNodePtr *Treeptr, int datanum);
 void print(TreeNodePtr treeptr);
 
 int main()
 {
     TreeNodePtr tree = NULL;
+    //struct treeNode *tree = NULL;
     int choice = 0;
-    int item = 0;
+    struct treeNode student;
+    // int item = 0;
+    printf("0 is add student\n1 is search student\n2 is print all students\n3 is Exit\n");
     printf("input your choice : ");
     scanf("%d", &choice);
     while (choice != 3)
     {
         switch (choice)
         {
-            case 0:
-                printf("insert a new node item in tree: ");
-                scanf("%d", &item);
-                insertNode(&tree, item);
-                break;
-            case 1:
-                // printf("search item in tree: ");
-                // scanf("%d", &item);
-                // searchNode(&tree, item);
-                break;
-            case 2:
-                printf("print all number(Low to High)");
-                print(tree);
-                puts("");
-                break;
-            default:
-                break;
+        case 0:
+            printf("insert a new student in tree: ");
+            scanf("%d %s %d %d %d", &student.datanumber, student.name, &student.score[0], &student.score[1], &student.score[2]);
+            insertNode(&tree, student.datanumber, student.name, student.score);
+            break;
+        case 1:
+            printf("search student in tree: ");
+            scanf("%d", &student.datanumber);
+            if (searchNode(&tree, student.datanumber) == 0)
+            {
+                printf("No Data Found!\n");
+            }
+            break;
+        case 2:
+            printf("print all number(Low to High)\n");
+            print(tree);
+            puts("");
+            break;
+        default:
+            break;
         }
+        printf("0 is add student\n1 is search student\n2 is print all students\n3 is Exit\n");
         printf("input your choice : ");
         scanf("%d", &choice);
     }
@@ -50,7 +61,7 @@ int main()
     return 0;
 }
 
-void insertNode(TreeNodePtr *Treeptr, int data)
+void insertNode(TreeNodePtr *Treeptr, int datanum, char stdname[20], int stdscore[])
 {
     if (*Treeptr == NULL)
     {
@@ -58,22 +69,35 @@ void insertNode(TreeNodePtr *Treeptr, int data)
 
         if (*Treeptr != NULL)
         {
-            (*Treeptr)->data = data;
+            (*Treeptr)->datanumber = datanum;
+            for (int i = 0; i < strlen(stdname); i++)
+            {
+                (*Treeptr)->name[i] = stdname[i];
+            }
+            for (int j = 0; j < 3; j++)
+            {
+                (*Treeptr)->score[j] = stdscore[j];
+            }
+
             (*Treeptr)->left = NULL;
             (*Treeptr)->right = NULL;
-        }else
-        {
-            printf("%d not inserted. No memory available.\n", data);
         }
-    }else
+        else
+        {
+            printf("student not inserted. No memory available.\n");
+        }
+    }
+    else
     {
-        if(data < (*Treeptr)->data)
+        if (datanum < (*Treeptr)->datanumber)
         {
-            insertNode(&((*Treeptr)->left), data);
-        }else if(data > (*Treeptr)->data)
+            insertNode(&((*Treeptr)->left), datanum, stdname, stdscore);
+        }
+        else if (datanum > (*Treeptr)->datanumber)
         {
-            insertNode(&((*Treeptr)->right), data);
-        }else
+            insertNode(&((*Treeptr)->right), datanum, stdname, stdscore);
+        }
+        else
         {
             printf("This data is duplication\n");
         }
@@ -82,10 +106,34 @@ void insertNode(TreeNodePtr *Treeptr, int data)
 
 void print(TreeNodePtr treeptr)
 {
-    if(treeptr != NULL)
+    if (treeptr != NULL)
     {
         print(treeptr->left);
-        printf("%d --> ", treeptr->data);
+        printf("%d %s %d %d %d\n", treeptr->datanumber, treeptr->name, treeptr->score[0], treeptr->score[1], treeptr->score[2]);
         print(treeptr->right);
+    }
+}
+
+int searchNode(TreeNodePtr *Treeptr, int data)
+{
+    if ((*Treeptr)->datanumber == data)
+    {
+        printf("%d %s %d %d %d\n", (*Treeptr)->datanumber, (*Treeptr)->name, (*Treeptr)->score[0], (*Treeptr)->score[1], (*Treeptr)->score[2]);
+        return 1;
+    }
+    else if ((*Treeptr)->left != NULL || (*Treeptr)->right != NULL)
+    {
+        if (data > (*Treeptr)->datanumber)
+        {
+            searchNode(&((*Treeptr)->right), data);
+        }
+        else
+        {
+            searchNode(&((*Treeptr)->left), data);
+        }
+    }
+    else
+    {
+        return 0;
     }
 }
